bfeb1b06c004a42286981750209c7f3f10274e7e
sisil031 <silfiardila96@gmail.com>
2025-11-25 16:22:12 +0700

    Update test2.ts

--- src/modules/modules_silfi/test2.ts
+++ src/modules/modules_silfi/test2.ts
@@ -5,18 +5,46 @@ export async function test2(page: Page) {
   console.log("\n▶️ Running TEST 2...");
 
   try {
-    await page.goto("https://demoqa.com/text-box", { waitUntil: "networkidle2" });
+    await page.goto("https://codenboxautomationlab.com/registration-form/", { waitUntil: "networkidle2" });
 
     await delay(300);
 
-    await page.type("#userName", "Automation Bot");
-    await page.type("#userEmail", "bot@test.com");
-    await page.type("#currentAddress", "Jl. Puppeteer Automation");
-    await page.type("#permanentAddress", "QA System");
+    await page.type("#nf-field-17", "ardila")
+    await delay(50);
+    console.log("Filled Name");
 
-    await delay(300);
+    await page.type("#nf-field-18", "putri")
+    await delay(50);
+    console.log("Last Filled Name");
 
-    await page.click("#submit");
+    await page.type("#nf-field-19", "ardila@gmail.com") 
+    await delay(50);
+    console.log("email");
+
+    await page.type("#nf-field-20", "0812683923") 
+    await delay(50);
+    console.log("phone");
+
+    await page.select('#nf-field-22', 'power-bi-and-sql');
+    await delay(50);
+    console.log("Selected course");
+
+    await page.select('#nf-field-24', 'march'); 
+    await delay(50);
+    console.log("Selected course");
+
+    await page.click("#nf-label-class-field-23-3");
+    await delay(50);
+    console.log("Clicked part time");
+
+    await page.click("#nf-field-15");
+    await delay(500);
+    console.log("Clicked submit button");
+
+  
+    await page.click("#nf-field-15");
+    await delay(500);
+    console.log("Clicked submit button");
 
     console.log("🚀 Test 2 completed successfully!");
 
